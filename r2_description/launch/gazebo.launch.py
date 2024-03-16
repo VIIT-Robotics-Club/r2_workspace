2300fa5f5d420a8217a3cf3a76fc37cd15efcb6a
Adyansh04 <gupta.adyansh@gmail.com>
2024-03-17 02:04:34 +0530

    ..

---
+++ r2_description/launch/gazebo.launch.py
@@ -0,0 +1,10 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+    pass
