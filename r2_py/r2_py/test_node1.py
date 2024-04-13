8fe7fad3132cd0e7c2637be8d2493efcfb2b2f00
Adyansh04 <gupta.adyansh@gmail.com>
2024-04-14 01:23:47 +0530

    Now we can also call luna_allign scripts using ps4 controller for manual operation

--- r2_py/r2_py/test_node1.py
+++ r2_py/r2_py/test_node1.py
@@ -4,6 +4,7 @@ from rclpy.node import Node
 import time
 import os
 from ament_index_python.packages import get_package_share_directory
+import subprocess
 
 
 package_path = get_package_share_directory('r2_bringup')
@@ -15,6 +16,19 @@ class TestNode1(Node):
         with open(file_path, 'w') as f:
             f.write('silo2')
 
+    def run_luna_wall_align(self):
+        print("Running luna wall align")
+        # cmd = "ros2 run luna_control luna_wall_align --ros-args -p silo_number:=2"
+        cmd = "ros2 run r2_py test_node2"
+        subprocess.Popen(cmd, shell=True)
+
+    def random_log_2sec_loop(self):
+        for i in range(3):
+            self.get_logger().info("Hello from random_log_2sec_loop")
+            time.sleep(2)
+        self.get_logger().info("Exiting after 3 iterations")
+      
+
         
 
 
@@ -22,6 +36,11 @@ class TestNode1(Node):
 def main(args=None):
     rclpy.init(args=args)
     node = TestNode1()
+
+    node.run_luna_wall_align()
+    node.random_log_2sec_loop()
+    print('Done')
+    rclpy.spin(node)
     rclpy.shutdown()
 
 
