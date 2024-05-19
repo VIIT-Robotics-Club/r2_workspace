d1c09264b13a85102017575ef25fc11d198c15ce
Pranav-engineer <engineer160904@gmail.com>
2024-05-19 13:52:08 +0530

    bot spwaning point fixed

--- r2_bringup/launch/simulation.py
+++ r2_bringup/launch/simulation.py
@@ -39,7 +39,7 @@ def generate_launch_description():
         package="gazebo_ros",
         executable="spawn_entity.py",
         arguments=["-topic", "/robot_description",
-                   "-entity", "R2"]
+                   "-entity", "tarm","-x 0","-y 0","-z 0.4","-R 0","-P 0","-Y 0"]
     )
     
     
